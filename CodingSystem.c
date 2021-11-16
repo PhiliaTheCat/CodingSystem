84bf97744e7bcdac8a3340fbd3819063425ebf44
SkyWolfy682 <2787974051@qq.com>
2021-11-16 09:38:40 +0800

    Several lines to remove repeats

--- CodingSystem.c
+++ CodingSystem.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-char *key;
-void inikey();
+void inikey(char *);
+void merge(char *);
 int main()
 {
-    int a;
-    inikey();
+    char *key = (char *)calloc(27, sizeof(char));
+    inikey(key);
+    int keysize = strlen(key);
 }
 
-void inikey()
+void inikey(char *key)
 {
-    key = (char *)malloc(sizeof(char) * 26);
+    printf("Please type in your key\nONE WORD ONLY\nNO SPECIAL SIGNAL ALLOWED\nKey: ");
+    scanf("%s", key);
+    merge(key);
+}
+
+void merge(char *key)
+{
     for (int i = 1; i <= 26; i += 1)
     {
-        scanf("%c", key + i - 1);
-        if (*(key + i - 1) == '\n')
-        {
-            *(key + i - 1) = '\0';
+        char check = *(key + i - 1);
+        if (check == 0)
             break;
+        for (int k = i + 1; k <= 26; k += 1)
+        {
+            if (check == *(key + k - 1))
+            {
+                *(key + k - 1) = 0;
+                strcat(key, (key + k));
+            }
         }
     }
-    
 }
