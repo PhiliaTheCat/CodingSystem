67bfa1060e777bbf0a33e7474aa9873d89497a3e
SkyWolfy682 <2787974051@qq.com>
2021-11-17 17:18:57 +0800

    A new menu for the system

--- CodingSystem.c
+++ CodingSystem.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void inikey(char *);
+int mainmenu();
+void getkey(char *);
 void merge(char *);
 int main()
 {
-    char *key = (char *)calloc(27, sizeof(char));
-    inikey(key);
-    int keysize = strlen(key);
+    int selected;
+    selected = mainmenu();
+    //char *key = (char *)calloc(27, sizeof(char));
+    //getkey(key);
 }
 
-void inikey(char *key)
+int mainmenu()
 {
-    printf("Please type in your key\nONE WORD ONLY\nNO SPECIAL SIGNAL ALLOWED\nKey: ");
+    int selected = -1;
+    while (selected < 0)
+    {
+        printf("----------------Coding System---------------\n");
+        printf("0 --- Encryption            1 --- Decryption\n");
+        switch (selected)
+        {
+            case -1:
+                printf("Please type in the number attached to your option\n");
+                break;
+            case -2:
+                printf("\033[0;31m");
+                printf("Invalid option input\n");
+                printf("\033[0m");
+                printf("Please type in the number again\n");
+                break;
+        }
+        printf("Option: ");
+        scanf("%d", &selected);
+        if (selected >= 2)
+        {
+            selected = -2;
+            system("cls");
+        }
+    }
+    return selected;
+}
+
+void getkey(char *key)
+{
+    printf("Please type in your key\nONE WORD ONLY\nNO SPECIAL SIGNAL ALLOWED\n");
+    printf("Key: ");
     scanf("%s", key);
     merge(key);
 }
