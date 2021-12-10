9557d30e27ac17fd71d08b23f8e90499ddb79242
flyingSpagetiMonstr <95331955+flyingSpagetiMonstr@users.noreply.github.com>
2021-12-10 21:36:31 +0800

    Update file_reader.c

--- file_reader.c
+++ file_reader.c
@@ -1,18 +1,39 @@
-//Don't forget to free(the returned char*)
+// Don't forget to free(the returned char*)
 #define _CRT_SECURE_NO_WARNINGS
+#define WITH_MAIN 0
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#if WITH_MAIN
 #include <ctype.h>
-#include <Windows.h>
+#endif
 
 #define EXIT_CHECK(pointer, str_sentence) \
     if (pointer == NULL)                  \
     {                                     \
-        puts(str_sentence "!!!");         \
+        puts(str_sentence);               \
         puts("Programe will exit.");      \
         system("PAUSE");                  \
         exit(EXIT_FAILURE);               \
+    }
+
+/*need pre-declared "int  i" and "char alone"*/
+#define STR_SUBTRACT(str, source, end_sign)                 \
+    i = 0;                                                  \
+    while (1)                                               \
+    {                                                       \
+        alone = source;                                     \
+        str = (char *)realloc(str, (i + 1) * sizeof(char)); \
+        EXIT_CHECK(str, #str "ALLOC FAILED")                \
+        if (alone != end_sign)                              \
+            str[i] = alone;                                 \
+        else                                                \
+        {                                                   \
+            str[i] = '\0';                                  \
+            break;                                          \
+        }                                                   \
+        i++;                                                \
     }
 
 char *read_file(void)
@@ -21,37 +42,19 @@ char *read_file(void)
     char *directory = NULL, *content = NULL;
     char alone;
     int i;
+
     puts("Enter the direcory to the source file pls:");
 
-    i = 0;
-    while (1)
-    {
-        alone = getchar();
-        directory = (char *)realloc(directory, (i + 1) * sizeof(char));
-        EXIT_CHECK(directory, "ALLOC FAILED")
-        if (alone != '\n')
-            directory[i] = alone;
-        else
-        {
-            directory[i] = '\0';
-            break;
-        }
-        i++;
-    }
+    STR_SUBTRACT(directory, getchar(), '\n')
 
     src_file = fopen(directory, "r");
     free(directory);
     EXIT_CHECK(src_file, "FILE OPEN FAILED");
 
-    i = 0;
-    while ((alone = fgetc(src_file)) != EOF)
-    {
-        content = (char *)realloc(content, (i + 1) * sizeof(char));
-        EXIT_CHECK(content, "ALLOC FAILED")
-        content[i] = alone;
-        i++;
-    }
-    content[i] = '\0';
+    STR_SUBTRACT(content, fgetc(src_file), EOF)
+
+    EXIT_CHECK((void *)(!fclose(src_file)), "FILE CLOSE FAILED")
+
     puts("The content of the file is shown as follows:");
     puts("\n");
 
@@ -60,3 +63,28 @@ char *read_file(void)
 
     return content;
 }
+
+#if WITH_MAIN
+int main()
+{
+    char choice;
+
+    while (1)
+    {
+        read_file();
+        puts("Read another file?(Y/N)");
+        choice = toupper(getchar());
+        if (choice == 'Y')
+        {
+            system("CLS");
+            setbuf(stdin, NULL);
+        }
+        else if (choice == 'N')
+        {
+            EXIT_CHECK(NULL, NULL)
+        }
+        else
+            EXIT_CHECK(NULL, "Wrong input, down to default:exit")
+    }
+}
+#endif
