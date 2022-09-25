9dafd1114c4af3140b6062efe75cbac3e8e8bdea
SkyWolfy682 <2787974051@qq.com>
2022-09-25 13:40:22 +0800

    String Proceeding

--- src/Cryptology.cpp
+++ src/Cryptology.cpp
@@ -1,46 +1,59 @@
 #include <iostream>
+#include <cstdlib>
 
 #include "../include/Cryptology"
 
 using namespace std;
 
-void Capitalize(char key[])
+void Capitalize(char str[])
 {
     int i = 0;
-    while (key[i] != 0)
+    while (str[i] != 0)
     {
-        if (key[i] >= 'a' && key[i] <= 'z')
-            key[i] -= 32;
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] -= 32;
     }
 }
 
-void Format(char key[])
+void Format(char str[])
 {
     bool flag[26] = {false};
     int i = 0, j = 0;
-    while (key[j] != 0)
+    while (str[j] != 0)
     {
-        if (key[i] == ' ')
+        if (str[i] == ' ')
             j += 1;
-        else if (flag[key[j] - 65] == false && i != j)
+        else if (flag[str[j] - 65] == false && i != j)
         {
             flag[i] = flag[j];
             i += 1;
             j += 1;
         }
-        else if (flag[key[j] - 65] == true)
+        else if (flag[str[j] - 65] == true)
             j += 1;
     }
 }
 
-bool Deny_Key()
+bool Deny_Str(char str[], int branch)
 {
-    char res;
-    cout << "Enter Y/N to Confirm/Deny: ";
-    cin.get(res);
-    cin.get();
-    if (res == 'Y' || res == 'y')
-        return true;
-    else 
-        return false;
+    while (1)
+    {
+        system("cls");
+        char res;
+        switch (branch)
+        {
+            case 0:
+                cout << "Enter Y/N to CONFIRM/DENY your key: ";
+                break;
+            case 1:
+                cout << "Enter Y/N to CONFIRM/DENY your message: ";
+                break;
+        }
+        cin.get(res);
+        cin.get();
+        if (res == 'Y' || res == 'y')
+            return true;
+        else if (res == 'N' || res == 'n')
+            return false;
+    }
 }
